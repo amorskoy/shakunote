1b6e5e9f2221c170d25870e2964a31168480b23c
Andrey Morskoy <cyclope@ya.ru>
2014-06-24 16:32:22 +0300

    +Add basic editor laout

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,36 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('SongCtrl', ['$scope', function($scope) {
 
+  .controller('SongCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+        $scope.lines = [];
+  
+        var createItem = function(note){
+            return {name:note.name, file:note.file};
+        }
+  
+        var addNote = function(note){
+            if(! $scope.lines.length)
+                addLine();
+            
+            var cnt = $scope.lines.length, line = $scope.lines[cnt-1];
+            
+            line.push( createItem(note) );
+        }
+        
+        var addLine = function(){
+            $scope.lines.push([]);
+        }
+  
+        $rootScope.$on('addNote', function(event, note) {
+            if(note.name=='newline')
+                addLine();
+            else
+                addNote(note);
+        });
   }])
-  .controller('ElementsCtrl', ['$scope', function($scope) {
+  
+  .controller('ElementsCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
           $scope.notes = [
               {name: 'ro_meri', title: 'RO meri', file: 'ro meri.png'}
               ,{name: 'ro', title: 'RO', file: 'ro.png'}
@@ -18,8 +44,13 @@ angular.module('myApp.controllers', [])
               ,{name: 'ha', title: 'HA', file: 'ha.png'}
               ,{name: 'hi', title: 'HI', file: 'hi.png'}
               ,{name: 'pre_meri', title: 'Pre meri', file: 'pre meri.png'}
+              ,{name: 'newline', title: 'Newline', file: 'new line.png'}
               ,{name: 'accent', title: 'Accent', file: 'accent.png'}
               ,{name: 'beat', title: 'Beat', file: 'beat.png'}
               ,{name: 'bar', title: 'Bar', file: 'bar.png'}
-          ]
+          ];
+          
+          $scope.addNote = function(note){
+              $rootScope.$broadcast('addNote', note);
+          }
   }]);
